d695dbcadcfa246a78024932c318c3e4a25d59e3
Sheryl Chee <sherylmichaela@gmail.com>
2023-12-20 18:40:30 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(result = "*") {
+  const innerFunction = function (emphatic = "special") {
+    return `You are ${result}${emphatic}${result}!`;
+  };
+  return innerFunction;
+}
+
+wrapAdjective()("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
